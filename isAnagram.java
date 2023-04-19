57d6d833fb482e341a162bfd6ad4432d29e1e1ec
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-04-19 16:34:21 -0400

    isAnagram

---
+++ isAnagram.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class isAnagram {
+    
+ /**
+
+    Logic:
+
+    1. check if both strings are of the same length, if not , return false
+    2. convert both strings to char arrays
+    3. sort both char arrays
+    4. return if their equals or not 
+     */
+    public boolean Anagram(String s, String t) {
+        
+        if(s.length() != t.length())return false;
+
+       char [] c = s.toCharArray();
+        Arrays.sort(c);
+       char [] c2 = t.toCharArray();
+       Arrays.sort(c2);
+
+    
+       return Arrays.equals(c,c2);  
+    }
+
+}
